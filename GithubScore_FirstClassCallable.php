010aea93aadcc8962c5f12e329b3ebde943cd8ae
Peter Markley <peter@itickets.com>
2023-10-23 17:02:19 -0400

    Add solution using first-class callable
    See also: https://www.php.net/manual/en/functions.first_class_callable_syntax.php

---
+++ GithubScore_FirstClassCallable.php
@@ -0,0 +1,44 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Illuminate\Support\Collection;
+
+class GitHubScore
+{
+    private $username;
+	
+    private function __construct($username)
+    {
+        $this->username = $username;
+    }
+	
+    public static function forUser($username)
+    {
+        return (new self($username))->score();
+    }
+	
+    private function score()
+    {
+        return $this->events()->pluck('type')->map($this->lookupScore(...))->sum();
+    }
+	
+    private function events()
+    {
+        //$url = "https://api.github.com/users/{$this->username}/events";
+        return collect(json_decode('[{"type":"PushEvent"}]', true));
+    }
+	
+    private function lookupScore($eventType)
+    {
+        debug_print_backtrace();
+        return collect([
+            'PushEvent' => 5,
+            'CreateEvent' => 4,
+            'IssuesEvent' => 3,
+            'CommitCommentEvent' => 2,
+        ])->get($eventType, 1);
+    }
+}
+
+var_dump(GitHubScore::forUser('PeterMarkley-iTickets'));
